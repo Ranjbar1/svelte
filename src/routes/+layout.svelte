c89615600f7b1f36d7b41a7f635aa66acc6a94e2
Abolfazl N.Ranjbar <98ranjbar@gmail.com>
2024-12-08 12:37:35 +0330

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="relative">
+	<nav class="sticky top-0 flex h-14 max-w-full items-center bg-pink-400">
+		<ul class="mx-[auto] flex w-[80%] gap-4">
+			<li><a href="/">home</a></li>
+			<li><a href="/about">about</a></li>
+		</ul>
+	</nav>
+	{@render children()}
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>about us</title>
+</svelte:head>
+
+<h1>hello there</h1>
